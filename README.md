d24597c5cc9e28c87538d93b1b966c2ef083f39b
saaim-siddiqui <git@stackblitz.com>
2025-04-01 06:18:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# recipe-list
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/saaim-siddiqui/recipe-list)
---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <h1>Hello from {{ name }}!</h1>
+    <a target="_blank" href="https://angular.io/start">
+      Learn more about Angular
+    </a>
+  `,
+})
+export class App {
+  name = 'Angular';
+}
+
+bootstrapApplication(App, {
+  providers: [provideExperimentalZonelessChangeDetection()],
+});
